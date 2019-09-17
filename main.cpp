8f437afba6149dedc8d1a572089fb30e8e033ed3
Frankleiton Levy <frankleiton@gmail.com>
2019-09-17 13:51:11 -0300

    Createe base binary tree

--- main.cpp
+++ main.cpp
@@ -2,6 +2,41 @@
 
 using namespace std;
 
+struct node{
+	int valor;
+	node *esquerda;
+	node *direita;
+};
+
+class Arvore_binaria
+{
+private:
+
+    node *cabeca;
+    
+public:
+    Arvore_binaria();
+    ~Arvore_binaria();
+
+    void inserir(int chave);
+    void remover(int chave);
+    node *buscar(int key);
+    void deletar_arvore();
+
+};
+
+Arvore_binaria::Arvore_binaria()
+{
+    this->cabeca = NULL;
+}
+
+Arvore_binaria::~Arvore_binaria()
+{
+    this->deletar_arvore();
+}
+
+
+
 int main(int argc, char const *argv[])
 {
     cout << "Projeto EDB2 - Arvores";
